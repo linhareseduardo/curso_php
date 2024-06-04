88d7e31a5111df5e81877d616acd0c37f7e0ae5c
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-04 11:45:22 -0300

    testando a função is_null()

---
+++ 3_tipos_de_dados/17_null/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    echo NULL;
+
+    $nome = NULL;
+
+    if(is_null($nome)){
+        echo "é nulo";
+    }
+
+    $nome = "Eduardo";
+    
+
+    if(is_null($nome)){
+        echo "O valor é nulo";
+    }
+
+?>
