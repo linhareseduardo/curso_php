e01a79db0a331e88607304b956f61a25969a05f3
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-04 11:33:39 -0300

    Exercicio 7 sobre arrray associativo

---
+++ 3_tipos_de_dados/15_exercicio_7/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+   $pessoa = ['nome' => 'Eduardo',
+              'idade' => 27, 
+              'altura' => 1.73];
+              
+   if($pessoa['idade'] >= 18){
+    echo $pessoa['nome'] . " é maior de idade";
+   }           
+
+?>
