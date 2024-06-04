5e63062951d2f81242912f6eaf3da3a0e2159be2
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-04 09:32:04 -0300

    Testando a função is_string()

---
+++ 3_tipos_de_dados/8_checando_strings/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+    $str = "Eduardo";
+    
+    $num = 27;
+
+    if(is_string($str)){
+        echo "$str é uma string 1 <br>";
+    }
+
+    if(is_string($num)){
+        echo "é uma string 2 <br>";
+    }
+
+    if(is_string("Texto")){
+        echo "é uma string 3 <br>"; 
+    }
+
+?>
