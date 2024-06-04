c42afcb8c3c17653fb77483443f8a7d2cf0567c3
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-04 10:47:15 -0300

    testando array e print_r()

---
+++ 3_tipos_de_dados/12_arrays/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $a = [1, 2, 3];
+
+    print_r($a);
+    echo "<br>";
+    echo $a[0];
+
+    $arr = ["Eduardo", 1, true];
+    echo "<br>";
+    print_r($arr);
+    echo "<br>";
+    echo $arr[2];
+?>
