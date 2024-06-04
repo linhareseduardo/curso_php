bb1048ced28a81b78758ea07ae22714ce12ca295
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-04 09:37:44 -0300

    Resolvendo exercicio 5 de strigs

---
+++ 3_tipos_de_dados/9_exercicio_5/index.php
@@ -0,0 +1,11 @@
+<?php 
+      $eu = "Eduardo";  
+      $pai = "João";
+      $mae = "Jackeline";
+      $irmao = "Guilherme";   
+
+      echo "Eu sou $eu, meu pai é $pai, minha mãe é $mae e meu irmão é $irmao <br>";
+      
+      echo 'Eu sou $eu, meu pai é $pai, minha mãe é $mae e meu irmão é $irmao';  
+
+?>
