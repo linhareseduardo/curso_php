22a407addf1b88a677511ce10a5cfa095bb380b3
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-04 11:40:53 -0300

    introdução a POO

---
+++ 3_tipos_de_dados/16_objetos/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    class Pessoa{
+        function falar(){
+            echo "Olá pessoal";
+        }
+    }
+
+    $eduardo = new Pessoa();
+
+    $eduardo->nome = "Eduardo";
+
+    echo $eduardo->nome;
+
+    echo "<br>";
+
+    $eduardo->falar();
+?>
