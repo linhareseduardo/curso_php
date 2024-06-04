b670d711a694b30b306baf1c06e2cc3102b58407
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-04 09:27:33 -0300

    Testando Strings, aspas simples e aspas duplas

---
+++ 3_tipos_de_dados/7_strings/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    echo "Testando texto de aspas duplas <br>";
+    echo 'Testando texto de aspas simples <br>';
+    echo "Ele disse 'olá' <br>";
+
+    echo 'Ele disse "olá" <br>';
+
+    $idade = 27;
+
+    echo "Ele tem $idade anos <br>";
+
+    echo 'Ele tem $idade anos <br>';
+
+?>
