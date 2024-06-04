d10a3b7bf007b7172f6695b8d0422480fed9b024
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-04 14:15:55 -0300

    iniciando novo módulo de variáveis e resolvendo o exercício 8

---
+++ 4_variaveis/1_criacao_de_var/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    $teste = "Algum texto";
+
+    echo $teste;
+
+    $num = 10;
+
+    echo "<br>";
+
+    echo $num;
+
+    $_nome = "Eduardo";
+
+    echo "<br>";
+    echo $_nome;
+
+?>
---
+++ 4_variaveis/2_exercicio_8/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $nome = "Eduardo";
+    $idade = 27;
+    $familia = ['pai' => 'João', 'mãe' => 'Jackeline', 'irmão' => 'Guilherme'];
+
+
+    echo $nome;
+    echo "<br>";
+    echo $idade;
+    echo "<br>";
+    print_r($familia);
+
+?>
