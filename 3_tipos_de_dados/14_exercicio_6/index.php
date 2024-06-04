199d38c73f5fd9a1a8206cde2d7c20d49ce8b833
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-04 11:26:12 -0300

    exercicio 6 sobre array

---
+++ 3_tipos_de_dados/14_exercicio_6/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $carro = ['nome' => 'Celta', 'marca' => 'Chevrolet', 'ano' => '2009', 'placa' => 'HVH9090'];
+    
+    print_r($carro);
+    echo "<br>";
+    echo $carro['nome'];
+    echo "<br>";
+    echo $carro['placa'];
+
+?>
