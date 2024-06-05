89e5070ec68ef3dd3023672b4c282a2d542ee19b
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 10:21:46 -0300

    operador mod

---
+++ 5_op_e_expressoes/7_op_modulo/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    echo 12 % 2;
+    echo " é o módulo de 12 % 2 <br>";
+    echo 12 / 2;
+    echo " é o resultado da divisão 12 / 2 <br>";
+
+    $a = 20;
+    $b = 4;
+    $c = 12;
+
+    echo $a % $b;
+    echo "<br>";
+    echo $a % $c;
+?>
