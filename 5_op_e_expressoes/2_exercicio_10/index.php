b227b706996fd4292f2334485bfefe8d30f1a6a6
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 09:17:51 -0300

    Resolução exercício 10 sobre operações matemáticas

---
+++ 5_op_e_expressoes/2_exercicio_10/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    $numero1 = 10;
+    $numero2 = 15;
+    $resultadoSubtr = $numero1 - $numero2;
+    $resultadoMult = $numero1 * $numero2;
+    $resultadoDiv = $numero1 / $numero2;
+    $resultadoOperacao = ($numero1 + $numero2) * ($numero2 - $numero1) / $numero1;
+    
+    echo $resultadoSubtr;
+    echo "<br>";
+    echo $resultadoMult;
+    echo "<br>";
+    echo $resultadoDiv;
+    echo "<br>";
+    echo $resultadoOperacao;    
+
+?>
