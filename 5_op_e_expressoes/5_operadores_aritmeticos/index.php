0a7fea934e6c792d70667bdb8fb7e24947890a94
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 09:46:08 -0300

    operadores aritméticos

---
+++ 5_op_e_expressoes/5_operadores_aritmeticos/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $soma = 5 + 5;
+    echo "$soma <br>";
+
+    $subtracao = 10 -2;
+    echo "$subtracao <br>";
+    
+    $multi = 2 * 10;
+    echo "$multi <br>";
+
+    $divisao = 50 / 8;
+    echo "$divisao <br>";
+?>
