9d736879a4a3241187ced641b7f6be5ed8fab8e3
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 10:30:37 -0300

    Resolução do exercicio 13 sobre resto de divisão

---
+++ 5_op_e_expressoes/8_exercicio_13/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $valor1 = 10;
+    $valor2 = 2;
+    $valor3 = 5;
+
+    $resultado = $valor1 % $valor2;
+    echo "O valor do resto da divisão entre $valor1 e $valor2 é: $resultado <br>";
+    $resultado = $valor3 % $valor2;
+    echo "O valor do resto da divisão entre $valor3 e $valor2 é: $resultado";
+
+
+
+?>
