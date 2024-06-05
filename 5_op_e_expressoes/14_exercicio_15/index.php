1d13d8222c1b6d6d4768e3dce62b1386b1ebef12
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 14:35:25 -0300

    Resolução do exercício 15

---
+++ 5_op_e_expressoes/14_exercicio_15/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+    $numero1 = 10;
+    $numero2 = 11;
+    $numero3 = 10;
+
+    if($numero1 == $numero2){
+        echo "Os números são iguais <br>";
+    }
+
+    if($numero1 != $numero2){
+        echo "Os números não são iguais <br>";
+    }
+
+    if($numero1 == $numero3){
+        echo "Os números são iguais <br>";
+    }
+
+?>
