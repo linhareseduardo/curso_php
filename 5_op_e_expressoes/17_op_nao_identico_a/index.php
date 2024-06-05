26ef4d07c2809ba0c5c7d40ddd334d228f97b557
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 14:53:03 -0300

    operador não é idêntico !==

---
+++ 5_op_e_expressoes/17_op_nao_identico_a/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    $a = 1;
+    $b = "1";
+    
+    if($a != $b){
+        echo "A é diferente de B 1 <br>";    
+    }
+
+    if($a !== $b){
+        echo "A é diferente de B 2 <br>";
+    }
+
+    if(1 !== 2){
+        echo "não é identico";    
+    }
+
+?>
