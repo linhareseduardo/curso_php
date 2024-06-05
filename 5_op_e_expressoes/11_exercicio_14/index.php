1dcb2f3afe68a0ee7ea7be43f3389c6624281cc9
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 14:00:52 -0300

    Resolução exercício 14 sobre concatenação de strings

---
+++ 5_op_e_expressoes/11_exercicio_14/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $saudacao = "Prazer em conhecer você";
+    $nome = "Eduardo";
+    $sobrenome = "Linhares";
+    $frase = "$saudacao meu nome é $nome $sobrenome";
+    
+    echo $frase;
+
+?>
