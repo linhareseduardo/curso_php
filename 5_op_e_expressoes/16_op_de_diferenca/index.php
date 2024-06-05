8301d2a6285570cd667693cd0109b41b4b742095
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 14:46:44 -0300

    operador de diferença

---
+++ 5_op_e_expressoes/16_op_de_diferenca/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+    $a = 3;
+    $b = 4;
+
+    if($a != $b){
+        echo "Testando diferença 1 <br>";
+    }
+
+    if($a != 3){
+        echo "Testando diferença 2 <br>";
+    }
+
+    if(false != "teste"){
+        echo "Testando diferença 3 <br>";
+    }
+
+
+
+?>
