dc93db0e3d88c86b9f5031b70997f5dc4306a2ec
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 15:06:45 -0300

    Resolução exercício 16

---
+++ 5_op_e_expressoes/18_exercicio_16/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+    $valor1 = 5;
+    $valor2 = 3;
+
+    if($valor1 == $valor2){
+        echo "$valor1 é igual a $valor2 <br>";
+    }
+
+    if($valor1 === $valor2){
+        echo "$valor1 é idêntico a $valor2 <br>";
+    }
+
+    if($valor1 != $valor2){
+        echo "$valor1 é diferente de $valor2 <br>";
+    }
+
+    if($valor1 !== $valor2){
+        echo "$valor1 não é idêntico a $valor2 <br>";
+    }
+
+?>
