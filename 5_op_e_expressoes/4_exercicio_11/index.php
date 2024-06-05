95f4d0782c95d1907ef7a62df1dfca2d36a25ff6
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 09:33:45 -0300

    Resolução exercício 11 sobre gettype()

---
+++ 5_op_e_expressoes/4_exercicio_11/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    echo "5" * 12;
+    echo "<br>";
+    $resultado = "5" * 12;
+    echo gettype($resultado);
+    echo "<br>";
+    echo gettype([]);
+    echo "<br>";
+    echo gettype(12.2);
+    echo "<br>";
+    echo gettype("12");
+
+?>
