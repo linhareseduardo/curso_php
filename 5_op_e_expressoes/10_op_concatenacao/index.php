a8b9041a69270082265ac294e2c1a0e3ce9c7c98
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-05 13:54:23 -0300

    Concatenação de string e variáveis

--- 5_op_e_expressoes/10_op_concatenacao/index.php
+++ 5_op_e_expressoes/10_op_concatenacao/index.php
@@ -5,5 +5,12 @@
     $t = "Testando";
     $c = "concatenação";
     echo $t . " a " . $c;
+    echo "<br>";
+    $marca = "Ferrari";
+    $motor = "3.0";
+    $vel_max = 200;
+
+    echo "O carro da marca " . $marca . " tem um motor " . $motor . " e chega uma velocidade " . $vel_max;
+
 
 ?>
