4b3d3877643f19b616f9d67edfef58fc239a1b9f
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-06 08:53:33 -0300

    operador or ||

---
+++ 5_op_e_expressoes/23_op_or/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+    if(5 > 2 || 3 < 4){// true e true
+        echo "A operação 1 é verdadeira <br>";
+    }
+
+    if(5 > 2 || 30 < 4){// true e false
+        echo "A operação 2 é verdadeira <br>";
+    }
+
+    if(5 > 20 || 3 < 4){// false e true
+        echo "A operação 3 é verdadeira <br>";
+    }
+
+    if(5 > 20 || 30 < 4){//false e false
+        echo "A operação 4 é verdadeira <br>";
+    }
+
+?>
