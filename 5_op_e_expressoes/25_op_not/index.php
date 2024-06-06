764eca69cd816bdeb398f41869f325959e03cf9c
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-06 09:24:43 -0300

    Operador not !

---
+++ 5_op_e_expressoes/25_op_not/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    if(!(5 > 2)){
+        echo "A operação 1 é verdadeira <br>"; 
+    }
+
+    if(!(5 > 20)){
+        echo "A operação 2 é verdaddeira <br>";
+    }
+
+    $a = 10;
+    $b = 20;
+
+    if(!($a >= $b)){
+        echo "A operação 3 é verdadeira <br>";    
+    }
+
+?>
