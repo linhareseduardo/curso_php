0e226974389de0d5c72c34721adbeeff973f2620
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-06 09:15:21 -0300

    Resolução do exercício 18

---
+++ 5_op_e_expressoes/24_exercicio_18/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    if(12 < 5 || "João" === "João"){
+        echo "operação 1 é verdade <br>";
+    }
+
+    if(1 > 5 || 1){
+        echo "operação 2 é verdade <br>";
+    }
+
+    if(20 === "20" && 51 >= 31){
+        echo "operação 3 é verdade <br>";
+    }
+
+?>
