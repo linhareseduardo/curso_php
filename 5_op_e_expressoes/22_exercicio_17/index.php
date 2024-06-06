f2bb4b301fc1e538c2d16fcf9d577720dd07a21a
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-06 08:47:15 -0300

    Resolução do exercício 17, sobre operador &&

---
+++ 5_op_e_expressoes/22_exercicio_17/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+    if(15 > 5 && "João" === "João"){
+        echo "Entrou no if 1 <br>";
+    }
+
+    if("teste" > 5 && 1){
+        echo "Entrou no if 2 <br>";
+    }
+
+    if(10 > 5 && 1){
+        echo "A comparação é verdadeira";
+    }
+
+    if(2 == 3 && 5 >= 3){
+        echo "Entrou no if 3 <br>";
+    }
+
+
+?>
