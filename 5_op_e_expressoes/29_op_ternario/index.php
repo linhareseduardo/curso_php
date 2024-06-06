10175b8503cb9280d443ee7e2956d06d8bfc59ce
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-06 14:34:23 -0300

    operador ternário

---
+++ 5_op_e_expressoes/29_op_ternario/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    echo 20 > 10 ? "Deu true <br>" : "Deu false <br>";
+
+    echo 20 > 50 ? "Deu true <br>" : "Deu false <br>";
+
+    $a = 10;
+    $b = 5;
+
+    echo $a >= $b ? "Deu true <br>" : "Deu false <br>";
+
+?>
