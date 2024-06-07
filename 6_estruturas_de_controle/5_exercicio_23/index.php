a2b1ff9cc9286b01261d3916ff34a988b4db0c2d
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-07 09:32:13 -0300

    Resolução exercício 23

---
+++ 6_estruturas_de_controle/5_exercicio_23/index.php
@@ -0,0 +1,13 @@
+<?php 
+    $idade = 17;
+    $maiorIdade = 18;
+    
+    if($idade >=$maiorIdade){
+        echo "Idade maior ou igual a 18 <br>";
+    }else{
+        echo "Idade menor que 18 <br>";
+    }
+
+
+
+?>
