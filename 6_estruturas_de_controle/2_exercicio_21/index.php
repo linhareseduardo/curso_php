07b266e1ff9515c1224473f2ade62ae891d4a267
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-07 08:31:43 -0300

    Resolução exercício 21 - estrutura de condição if

---
+++ 6_estruturas_de_controle/2_exercicio_21/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+    $a = 5;
+    $b = 2;
+    $c = "Matheus";
+    $d = "Pedro";
+    $e = 12;
+    $f = 11;
+
+    if($a > $b){
+        echo "Entrou no if 1 <br>";    
+    }   
+    
+    if($c != $d){
+        echo "Entrou no if 2 <br>";    
+    }
+
+    if($e <= $f){
+        echo "Entrou no if 3 <br>";
+    }
+
+?>
