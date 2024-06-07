87286d615a183afb7ab507334b549fe0f9db1b95
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-07 08:37:40 -0300

    Resolução exercício 22 - estruturas de condição if com variáveis

---
+++ 6_estruturas_de_controle/3_exercicio_22/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+    $idade = 18;
+    $idade2 = 15;
+    $idade3 = 27;
+    $maioridade = 18;
+
+    if($idade >= $maioridade){
+        echo "1 A idade é maior ou igual a 18 <br>";
+    }
+
+    if($idade2 >= $maioridade){
+        echo "2 A idade é maior ou igual a 18 <br>";
+    }
+
+    if($idade3 >= $maioridade){
+        echo "3 A idade é maior ou igual a 18 <br>";
+    }
+
+?>
