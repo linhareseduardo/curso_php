4134339a21de3cd4361b9e2d78cc5c3c8e450b04
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-07 10:29:15 -0300

    Resolução do exercício 26 completa

--- 6_estruturas_de_controle/11_exercicio_26/index.php
+++ 6_estruturas_de_controle/11_exercicio_26/index.php
@@ -1,9 +1,12 @@
 <?php 
 
-    $vel = 36;
+    $vel = 40;
+    $velMax = 40;
 
     if($vel < 40){
         echo "Parabéns você esta numa velocidade segura <br>";    
+    }else if($vel == $velMax){
+        echo "Cuidado! Você está no limite de velocidade <br>";       
     }
 
 ?>
