7a49068bc47a65f094d14b4f1b04ebd3686d51ff
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-07 09:27:38 -0300

    estrutura de controle else

---
+++ 6_estruturas_de_controle/4_else/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+    if(5 > 2){
+        echo "Entrou no if 1<br>";        
+    }
+    else{
+        echo "Entrou no else 1<br>";
+    }
+
+    // variáveis
+
+    $a = 10;
+    $b = 20;
+
+    if($a > $b){
+
+    }else{
+        echo "Entrou no else 2 <br>";
+    }
+
+    $msg = "Entrou no else 3 <br>";
+
+    if($a > $b){
+
+    }else{
+        echo $msg;
+    }
+
+
+
+?>
