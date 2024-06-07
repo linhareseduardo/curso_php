d4d09a1f3bcb40628b90f04e6a97b311333acfdc
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-07 10:44:10 -0300

    Switch, case e default

---
+++ 6_estruturas_de_controle/12_switch/index.php
@@ -0,0 +1,32 @@
+<?php 
+
+    $x = 0;
+    
+    switch($x){
+        case 0:
+            echo "X é igual a 0 <br>";
+            break;
+        case 1:
+            echo "X é igual a 1 <br>";        
+            break;
+        default:
+            echo "X não é igual a nenhum dos valores mencionados <br>";    
+    }
+
+    $y = "Eduardo";
+
+    switch($y){
+        case "João":
+            echo "O nome é João";
+            break;
+        case "Eduardo":
+            echo "O nome é Eduardo";
+            break;
+        default:
+            echo "O nome não foi encontrado";    
+
+    }
+
+
+
+?>
