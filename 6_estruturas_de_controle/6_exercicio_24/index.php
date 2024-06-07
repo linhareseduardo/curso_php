a37d319e915dba19cfcea9ab68f035f98166a4da
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-07 09:36:41 -0300

    Resolução exercício 24

---
+++ 6_estruturas_de_controle/6_exercicio_24/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+    $a = 10;
+    $b = "Eu";
+    $t  = true;
+
+    if(is_int($a)){
+        echo "A variável é um inteiro <br>";    
+    }else{
+        echo "A variável não é um interiro <br>";
+    }
+
+    if(is_int($b)){
+        echo "A variável é um inteiro <br>";    
+    }else{
+        echo "A variável não é um interiro <br>";
+    }
+
+    if(is_int($t)){
+        echo "A variável é um inteiro <br>";    
+    }else{
+        echo "A variável não é um interiro <br>";
+    }
+
+
+?>
