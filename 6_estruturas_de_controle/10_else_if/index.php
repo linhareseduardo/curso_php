85d30017c1d4838768c0442a97de6064984dceae
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-07 10:20:06 -0300

    else if

---
+++ 6_estruturas_de_controle/10_else_if/index.php
@@ -0,0 +1,25 @@
+<?php 
+
+    if(10 > 5){
+        echo "Entrou no if 1 <br>";
+    }else if(10 > 6){
+        echo "Entrou no else if 1 <br>";
+    }
+
+    if(10 < 5){
+        echo "Entrou no if 2 <br>";
+    }else if(10 > 6){
+        echo "Entrou no else if 2 <br>";
+    }
+
+    if(10 < 5){
+        echo "Entrou no if 3 <br>";
+    }else if(10 < 6){
+        echo "Entrou no else if 3 <br>";
+    }else if(11 > 1){
+        echo "Entrou no else if 2 4 <br>";
+    }else{
+        echo "Entrou no else 3 <br>";
+    }
+
+?>
