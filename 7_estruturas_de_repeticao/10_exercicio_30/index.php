ba166c34d1db0395e4ac2ebcc4c78b54f98ade9d
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-14 13:40:02 -0300

    Resolução exercício 30

---
+++ 7_estruturas_de_repeticao/10_exercicio_30/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    $array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    for($i=0; $i < count($array); $i++){
+        if($array[$i] % 2 == 0){
+            echo $array[$i] . "<br>";
+        }else{
+            echo "ímpar <br>";
+        }    
+    }
+
+
+?>
