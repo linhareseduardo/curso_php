f5dbfb48d12446d18a3d54bc0e49840d9996a22d
linhareseduardo <linhareseduardo7@gmail.com>
2024-06-14 14:36:21 -0300

    loop foreach

---
+++ 7_estruturas_de_repeticao/14_foreach/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $nomes = ["Matheus", "João", "Pedro", "Maria"];
+
+    foreach($nomes as $nome){
+        echo "O nome do índice atual é $nome <br>";
+        if($nome == "Matheus"){
+            echo "Olá <br>";
+        }    
+    }
+
+
+
+?>
